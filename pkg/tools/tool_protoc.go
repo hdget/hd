96eecca173667abdaded97c1b778488970c149e0
rfancn <reg_info@qq.com>
2025-04-07 18:14:28 +0800

    debug

--- pkg/tools/tool_protoc.go
+++ pkg/tools/tool_protoc.go
@@ -11,14 +11,20 @@ type protocTool struct {
 }
 
 const (
-	urlProtocWindowsBinary = "https://github.com/protocolbuffers/protobuf/releases/download/v%s/protoc-%s-win64.zip"
+	urlProtoc = "https://github.com/protocolbuffers/protobuf/releases/download/v%s/%s"
 )
 
 func Protoc() Tool {
+	version := "30.2"
+	winfile := fmt.Sprintf("protoc-%s-win64.zip", version)
+	linuxFile := fmt.Sprintf("protoc-%s-linux-x86_64.zip", version)
+
 	return &protocTool{
 		toolImpl: &toolImpl{
-			name:    "protoc",
-			version: "30.2",
+			name:            "protoc",
+			version:         version,
+			urlLinuxRelease: fmt.Sprintf(urlProtoc, version, linuxFile),
+			urlWinRelease:   fmt.Sprintf(urlProtoc, version, winfile),
 		},
 	}
 }
@@ -29,22 +35,36 @@ func (t *protocTool) IsInstalled() bool {
 }
 
 func (t *protocTool) LinuxInstall() error {
-	cmd := `(curl -L https://github.com/protocolbuffers/protobuf/releases/latest/download/protoc-*.zip -o protoc.zip && \
-                     unzip -o protoc.zip -d /usr/local && \
-                     rm protoc.zip) || (echo "安装失败，请手动下载: https://github.com/protocolbuffers/protobuf/releases" && exit 1)`
+	fmt.Printf("正在下载%s...\n", t.name)
 
-	output, err := script.Exec(cmd).String()
+	tempDir, zipFile, err := AllPlatform().Download(t.urlLinuxRelease)
 	if err != nil {
-		return fmt.Errorf("protoc安装失败: %v\n输出: %s", err, output)
+		return err
+	}
+	defer func() {
+		if e := os.RemoveAll(tempDir); e != nil {
+			fmt.Printf("删除临时目录失败: %v, dir: %s", e, tempDir)
+		}
+	}()
+
+	// 获取GOPATH
+	installDir, err := AllPlatform().GetGoBinDir()
+	if err != nil {
+		return err
+	}
+
+	// 解压zip文件
+	if err = AllPlatform().UnzipSpecific(zipFile, "bin/protoc", installDir); err != nil {
+		return err
 	}
 
 	return nil
 }
 
 func (t *protocTool) WindowsInstall() error {
-	fmt.Printf("正在下载%s v%s...\n", t.name, t.version)
-	url := fmt.Sprintf(urlProtocWindowsBinary, t.version, t.version)
-	tempDir, zipFile, err := AllPlatform().Download(url)
+	fmt.Printf("正在下载%s...\n", t.name)
+
+	tempDir, zipFile, err := AllPlatform().Download(t.urlWinRelease)
 	if err != nil {
 		return err
 	}
