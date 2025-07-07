231f1351ae6c370f6e371ea66e64cfa9f2f2d2a1
rfancn <reg_info@qq.com>
2025-07-07 13:15:08 +0800

    change app stop

--- pkg/appctl/appctl_stop_unix.go
+++ pkg/appctl/appctl_stop_unix.go
@@ -8,7 +8,6 @@ import (
 	"github.com/spf13/cast"
 	"os"
 	"syscall"
-	"time"
 )
 
 const (
@@ -22,36 +21,27 @@ func sendStopSignal(strDaprdPid, strAppPid string) error {
 		return fmt.Errorf("invalid pid, daprdPid: %s, appPid: %s", strDaprdPid, strAppPid)
 	}
 
-	appProcess, err := os.FindProcess(appPid)
-	if err != nil {
-		return errors.Wrapf(err, "find app process, pid: %d", appPid)
-	}
-
 	// 给app进程发SIGUSR1标识stop信号
-	err = appProcess.Signal(syscall.SIGUSR1)
-	if err != nil {
-		return errors.Wrapf(err, "send app process stop signal, pid: %d", appPid)
-	}
+	if process, _ := os.FindProcess(appPid); process != nil {
+		err = process.Signal(syscall.SIGUSR1)
+		if err != nil {
+			return errors.Wrapf(err, "send app process stop signal, pid: %d", appPid)
+		}
 
-	// 等待app stop
-	for i := appQuitCountdown; i > 0; i-- {
-		fmt.Printf("wait app stop: %d second\n", i)
-		time.Sleep(1 * time.Second) // 阻塞 1 秒
+		// 等待app stop
+		for i := appQuitCountdown; i > 0; i-- {
+			fmt.Printf("wait app stop: %d seconds\n", i)
+			time.Sleep(1 * time.Second) // 阻塞 1 秒
+		}
 	}
 
 	// 给daprd发送stop信号
-	daprdProcess, err := os.FindProcess(daprdPid)
-	if err != nil {
-		return errors.Wrapf(err, "find daprd process, pid: %d", daprdPid)
+	if process, _ := os.FindProcess(daprdPid); process != nil {
+		err = daprdProcess.Signal(syscall.SIGTERM)
+		if err != nil {
+			return errors.Wrapf(err, "send daprd process terminal signal, pid: %d", daprdPid)
+		}
 	}
-
-	err = daprdProcess.Signal(syscall.SIGTERM)
-	if err != nil {
-		return errors.Wrapf(err, "send daprd process terminal signal, pid: %d", daprdPid)
-	}
-
-	fmt.Println("app:", appProcess.Pid)
-	fmt.Println("daprd:", daprdProcess.Pid)
 
 	return nil
 }
